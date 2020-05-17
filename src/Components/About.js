7a2c61d7a4e73813f18397f1291cfbc34d7d01ea
Sari Stissi <sestissi16@earlham.edu>
2020-05-17 16:47:03 -0500

    changed div containers and added ids to be able to use flexbox instead of px values

--- src/Components/About.js
+++ src/Components/About.js
@@ -18,36 +18,36 @@ class About extends Component {
 
     return (
       <section id="about">
-      <div className="row">
-         <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
-         </div>
-         <div className="nine columns main-col">
-            <h2>About Me</h2>
-
-            <p>{bio}</p>
-            <div className="row">
-               <div className="columns contact-details">
-                  <h2>Contact Details</h2>
-                  <p className="address">
-						   <span>{name}</span><br />
-						   <span>{street}<br />
-						         {city} {state}, {zip}
-                   </span><br />
-						   <span>{phone}</span><br />
-                     <span>{email}</span>
-					   </p>
+         <div id="aboutContainer">
+            <div id="aboutPictureContainer">
+               <img className="profile-pic"  src={profilepic} alt="Sari Stissi Profile Pic" />
+            </div>
+            <div id="aboutMeDetails">
+               <div id="aboutMeInfo">
+                  <h2>About Me</h2>
+                  <p>{bio}</p>
                </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
+               <div id="aboutContactResumeRow">
+                  <div id="aboutContactDetails">
+                     <h2>Contact Details</h2>
+                     <p className="address">
+                        <span>{name}</span><br />
+                        <span>{street}<br />
+                              {city} {state}, {zip}
+                     </span><br />
+                        <span>{phone}</span><br />
+                        <span>{email}</span>
+                     </p>
+                  </div>
+                  <div id="aboutDownload">
+                     <p>
+                        <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
+                     </p>
+                  </div>
                </div>
             </div>
          </div>
-      </div>
-
-   </section>
+      </section>
     );
   }
 }
