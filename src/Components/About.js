593ae4ba096f4cea7b4a3946c852da20f84bd299
Sari Stissi <sestissi16@earlham.edu>
2020-06-23 13:25:51 -0700

    styled, made responsive, added bio, removed street from address

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,11 +6,11 @@ class About extends Component {
     if(this.props.data){
       var name = this.props.data.name;
       var profilepic= "images/"+this.props.data.image;
-      var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
+      var bio1 = this.props.data.bio1;
+      var bio2 = this.props.data.bio2;
+      var bio3 = this.props.data.bio3;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
       var email = this.props.data.email;
       var resumeDownload = this.props.data.resumedownload;
@@ -25,16 +25,20 @@ class About extends Component {
             <div id="aboutMeDetails">
                <div id="aboutMeInfo">
                   <h2>About Me</h2>
-                  <p>{bio}</p>
+                  <p className="bio">
+                     {bio1}<br/><br/>
+                     {bio2}<br/><br/>
+                     {bio3}
+                  </p>
                </div>
                <div id="aboutContactResumeRow">
                   <div id="aboutContactDetails">
                      <h2>Contact Details</h2>
                      <p className="address">
                         <span>{name}</span><br />
-                        <span>{street}<br />
-                              {city} {state}, {zip}
-                     </span><br />
+                        <span>
+                              {city}, {state}
+                        </span><br />
                         <span>{phone}</span><br />
                         <span>{email}</span>
                      </p>
